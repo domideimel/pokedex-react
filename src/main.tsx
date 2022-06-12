8e79bc95e56e4152389cb619ddd296c542223fda
dominikdeimel <info@dominikdeimel.com>
2022-06-12 14:38:46 +0200

    chore: added chakraui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+  <StrictMode>
+    <ChakraProvider>
+      <App/>
+    </ChakraProvider>
+  </StrictMode>
 )
