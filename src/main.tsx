9bb2eca3c43a66883e9e06692f49f3266761f669
dominikdeimel <info@dominikdeimel.com>
2022-06-12 14:44:52 +0200

    chore: added darkmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
+
+const theme = extendTheme({ config })
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App/>
     </ChakraProvider>
   </StrictMode>
